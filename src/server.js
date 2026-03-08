cd1b4b8dc1ac4b7eb94080db1b3352be68969f7d
Krost-t <christ.matsanganzoulou@gmail.com>
2026-03-08 19:13:38 +0100

    feat(server): add Express app with CORS and graceful shutdown

--- src/server.js
+++ src/server.js
@@ -1,9 +1,60 @@
 import express from "express"
+import cors from "cors";
+import {config} from "dotenv"
+import { connectDB, disconnectDB } from "./config/db.js"
+
+
+// Import Routes
+import { movieRoutes } from "./routes/movie.routes.js"
+import { authRoutes } from "./routes/auth.routes.js"
+
+config()
+connectDB()
 
 const PORT = 5001
 const app = express()
 
 
-app.listen(PORT, () => {
+// Body parsing middlwares
+app.use(cors());
+ app.use(express.json())
+ app.use(express.urlencoded({extended: true}))
+
+
+// API Routes
+app.use("/movies", movieRoutes)
+app.use("/auth", authRoutes)
+
+
+
+
+const server = app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
+})
+
+
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1);
+  });
+});
+
+
+
+process.on("uncaughtException", (err) => {
+    console.error("Unhandled Exception:", err)
+    server.close(async () => {
+        await disconnectDB()
+        process.exit(1)
+    })
+})
+
+process.on("SIGTERM", (err) => {
+    console.error("SIGTERM received, shutting down gracefully")
+    server.close(async () => {
+        await disconnectDB()
+        process.exit(0)
+    })
 })
